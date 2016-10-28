b6c721d0fac88cfa95d56e47fd5330817796b2d8
Lansky <github_dedicated@163.com>
2016-10-28 12:15:54 +0800

    add annotation

--- Lansky/src/test/designPattern/proxy/virtualProxy/Client.java
+++ Lansky/src/test/designPattern/proxy/virtualProxy/Client.java
@@ -2,6 +2,13 @@ package test.designPattern.proxy.virtualProxy;
 
 import java.util.List;
 
+/** 
+ * @ClassName: Client 
+ * @Description: 虚拟代理
+ * @author Lansky
+ * @date 2016年10月28日 下午12:13:55 
+ *  
+ */
 public class Client {
 	public static void main(String[] args) {
 		Dept dept = new Dept();
@@ -10,7 +17,7 @@ public class Client {
 		List<UserInterface> list = userManager.getUserListByDept(dept);
 		UserInterface user1 = list.get(0);
 		System.out.println(user1.getUserId());
-		System.out.println(user1.getName());
+		System.out.	println(user1.getName());
 		System.out.println(user1.getDeptId());
 		System.out.println(user1.getSex());
 		System.out.println(user1.getJob());
