4d9e969f814e7f3fdcb1bd7991cd3a1b28c2649f
Lansky <github_dedicated@163.com>
2016-10-29 10:13:29 +0800

    optimize code

--- Lansky/src/test/designPattern/observer/jdkPackage/Reader.java
+++ Lansky/src/test/designPattern/observer/jdkPackage/Reader.java
@@ -6,10 +6,10 @@ import java.util.Observer;
 public class Reader implements Observer {
 	private String name;
 
-	//两个参数，一个是pull方式传过来的对象，另一个是push方式推送过来的参数
+	//两个参数，第一个是pull方式传过来的对象，第二个是push方式推送过来的参数
 	@Override
 	public void update(Observable pull_obj, Object push_argument) {
-		// TODO Auto-generated method stub
+		
 		System.out.println(this.getName() + "---接到报纸更新通知，内容为： " + push_argument);
 	}
 
